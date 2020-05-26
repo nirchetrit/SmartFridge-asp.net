ad119fb6c39c1327176445a2bd9fd79cedc08aec
nirchetrit <54546127+nirchetrit@users.noreply.github.com>
2020-05-26 10:48:39 +0300

    Add project files.

---
+++ FridgeStockWeb/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FridgeStockWeb.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [DisplayName("Name")]
+        public string name { get; set; }
+  
+        [DisplayName("Last updated date")]
+        [DataType(DataType.Date)]
+        public DateTime dateOfLastUpdate { get; set; }
+
+        [DisplayName("Priority")]
+        public int? priority { get; set; }
+
+        public int Count { get; set; }
+
+        public string UserID { get; set; }
+    }
+}
